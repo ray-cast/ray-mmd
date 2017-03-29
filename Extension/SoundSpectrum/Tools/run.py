f73b536823964710216afd4d7d080b8e13597794
ray-cast <2221870259@qq.com>
2017-03-29 12:04:22 +0800

    Add SoundSpectrum

---
+++ Extension/SoundSpectrum/Tools/run.py
@@ -0,0 +1,141 @@
+import sys
+
+from pylab import*
+from scipy.io import wavfile
+from PIL import Image, ImageDraw
+
+FPS = 30
+
+TEXTURE_WIDTH = 256
+TEXTURE_HEIGHT = 256
+
+FFT_SAMPLE_WAVE_1 = 0.01
+FFT_SAMPLE_WAVE_2 = 0.02
+FFT_SAMPLE_WAVE_3 = 0.03
+FFT_SAMPLE_WAVE_4 = 0.04
+FFT_SAMPLE_WAVE_5 = 0.05
+FFT_SAMPLE_WAVE_6 = 0.06
+FFT_SAMPLE_WAVE_7 = 0.07
+FFT_SAMPLE_WAVE_8 = 0.08
+
+FFT_SCOPE = 1024
+FFT_UNIFORM = 150
+
+def FFT(s1):
+    global fft_max
+    n = len(s1) 
+    p = fft(s1)
+    nUniquePts = int(ceil((n+1)/2.0))
+    p = p[0:nUniquePts]
+    p = abs(p)
+    p = p / float(n)
+    p = p**2
+
+    if n % 2 > 0:
+        p[1:len(p)] = p[1:len(p)] * 2
+    else:
+        p[1:len(p) -1] = p[1:len(p) - 1] * 2
+
+    ret = [abs(10*log10(x+1e-5)) for x in p]
+    return ret
+
+def gen(fn):
+    sampFreq, snd = wavfile.read(fn)
+
+    snd = snd / (2.**15)
+    soundLength = len(snd) / sampFreq
+    print "INFO: Sound length = ", soundLength / FPS
+
+    s1 = snd[:,0]
+
+    print "INFO: Left channel size =", len(s1)
+    
+    img1 = Image.new("RGBA", (TEXTURE_WIDTH,TEXTURE_HEIGHT), color=(0,0,0,0))
+    img2 = Image.new("RGBA", (TEXTURE_WIDTH,TEXTURE_HEIGHT), color=(0,0,0,0))
+    
+    draw1 = ImageDraw.Draw(img1)
+    draw2 = ImageDraw.Draw(img2)
+    
+    FFT_STEP = sampFreq / FPS
+    
+    limit = 0.0;
+    
+    lastValue1 = 0.0;
+    lastValue2 = 0.0;
+    
+    rangeLimits = int(soundLength*FPS);
+    
+    for i in xrange(rangeLimits):
+        start_index = FFT_STEP*i
+        end_index = start_index + FFT_SCOPE
+
+        if  end_index >= len(s1):
+            end_index = len(s1) - 1
+            
+        if (start_index > 0.5 * FFT_STEP):
+            start_index -= 0.5 * FFT_STEP
+            end_index -= 0.5 * FFT_STEP
+
+        fft_data = s1[int(start_index):int(end_index)]
+        fft_ret = FFT(fft_data)
+        fft_ret_len = len(fft_ret)
+
+        rgba = [fft_ret[int(FFT_SAMPLE_WAVE_5*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_6*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_7*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_8*fft_ret_len)]]
+
+        for x in rgba:
+            limit = max(limit, x)
+
+    print "INFO: Max limit = ", limit
+
+    for i in xrange(rangeLimits):
+        start_index = FFT_STEP*i
+        end_index = start_index + FFT_SCOPE
+
+        if  end_index >= len(s1):
+            end_index = len(s1)-1
+            
+        if (start_index > 0.5 * FFT_STEP):
+            start_index -= 0.5 * FFT_STEP
+            end_index -= 0.5 * FFT_STEP
+
+        fft_data = s1[int(start_index):int(end_index)]
+        fft_ret = FFT(fft_data)
+        fft_ret_len = len(fft_ret)
+
+        rgba1 = [fft_ret[int(FFT_SAMPLE_WAVE_1*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_2*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_3*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_4*fft_ret_len)]]
+                
+        rgba2 = [fft_ret[int(FFT_SAMPLE_WAVE_5*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_6*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_7*fft_ret_len)],
+                fft_ret[int(FFT_SAMPLE_WAVE_8*fft_ret_len)]]
+
+        rgba1 = [255 * (1.0 - x / limit) for x in rgba1]
+        rgba2 = [255 * (1.0 - x / limit) for x in rgba2]
+
+        lastValue1 = rgba1;
+        lastValue2 = rgba2;
+
+        px = i % TEXTURE_WIDTH;
+        py = i / TEXTURE_HEIGHT;
+
+        draw1.point((px, py), fill=(int(rgba1[0]), int(rgba1[1]), int(rgba1[2]), int(rgba1[3])))
+        draw2.point((px, py), fill=(int(rgba2[0]), int(rgba2[1]), int(rgba2[2]), int(rgba2[3])))
+
+    img1.save(fn+"1.png", 'png')
+    img2.save(fn+"2.png", 'png')
+    
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        fn = sys.argv[1]
+        gen(fn)
+        print 'DONE...'
+    else:
+        print "ERROR! Please enter a path to a wav file..."
+
+    raw_input()
---
+++ Extension/SoundSpectrum/WaveLines/Experiment by IQ/WaveLines.fx
@@ -0,0 +1,25 @@
+#define USE_CUSTOM_PARAMS 0
+
+static float waveLines = 8;        // 0 ~ 8
+static float waveBloom = 3;        // 1 ~ inf
+static float waveHeightLow = 5;    // 1 ~ inf
+static float waveHeightHigh = 7;   // 1 ~ inf
+static float waveFade = 2;         // 0 ~ inf
+static float waveSin = 10;         // 1 ~ inf
+static float waveCos = 2;          // 1 ~ inf
+static float waveWheel = 1;        // 1 ~ inf
+static float waveOffset = 0.0;     //-1 ~ 1
+
+static float3 waveColorLow  = float3(0.63, 1.0, 1.0); // hsv, h & s 0 ~ 1, v 0 ~ inf
+static float3 waveColorHigh = float3(0.0 , 1.0, 1.7); // hsv, h & s 0 ~ 1, v 0 ~ inf
+
+static float2 size = 1.0;          // 0 ~ 1
+static float2 translate = 0.0;     // 0 ~ 1
+
+// ignore USE_CUSTOM_PARAMS
+#define USE_RGB_SPACE 0
+
+#define WAVE_1_MAP_FILE "spectrum.wav1.png"
+#define WAVE_2_MAP_FILE "spectrum.wav2.png"
+
+#include "wavelines.fxsub"
